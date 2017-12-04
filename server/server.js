55a65a10ac17b818982dd1735123ed07c2b1c3d1
CW <you@example.com>
2017-12-04 09:43:10 -0500

    Added socket.io modules

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const path = require('path');
-
+const http = require('http');
+const socketIO = require('socket.io');
 const express = require('express');
+
 const port = process.env.PORT || 3000;
 var app = express();
+
+var server = http.createServer(app);
+
+
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
+var io = socketIO(server);
+
+io.on('connection', (socket) => {   //registers event listenter 'connection'
+console.log("New User Connected");
+
+socket.on('disconnect', () => {
+console.log("Server was disconnected");
+});
+});
 
 
 
 
 
-
-
-app.listen(port, () => {
+server.listen(port, () => {
 console.log(`Server is running on ${port}`);
 });
 
